8f29421f546b4577f347229fb4ecc9fe25c533c0
razvan <razvanstefan.manole@studenti.univr.it>
2024-06-20 18:08:55 +0200

    put another method

--- src/main/java/com/univr/runnerz/run/RunController.java
+++ src/main/java/com/univr/runnerz/run/RunController.java
@@ -1,14 +1,18 @@
 package com.univr.runnerz.run;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/runs")
@@ -31,6 +35,10 @@ public class RunController {
     //in order to get the id from getmapping we need the pathVariable
     @GetMapping("/{id}")
     Run findById(@PathVariable Integer id){
-        return runRepository.findById(id);
+        Optional<Run> run =runRepository.findById(id);
+        if(run.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found.");
+        }
+        return run.get();
     }
 }
--- src/main/java/com/univr/runnerz/run/RunRepository.java
+++ src/main/java/com/univr/runnerz/run/RunRepository.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class RunRepository {
@@ -16,11 +17,10 @@ public class RunRepository {
         return runs;
     }
 
-    Run findById(Integer id){
+    Optional<Run> findById(Integer id){
         return  runs.stream()
                 .filter(run -> run.id() == id)
-                .findFirst()
-                .get();
+                .findFirst();
     }
 
     //will do some initialization
